e0de81e233a9e31e2856ae9943a8ec129b986bfd
Mamello <mmoshoeshoe@webelieve.co.za>
2024-11-26 20:52:48 +0200

    add article section

---
+++ src/app/components/article-section/article-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-section.component.html',
+  styleUrl: './article-section.component.scss'
+})
+export class ArticleSectionComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
 import { FeaturesComponent } from '../../components/features/features.component';
+import { ArticleSectionComponent } from '../../components/article-section/article-section.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
     BannerComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    ArticleSectionComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'

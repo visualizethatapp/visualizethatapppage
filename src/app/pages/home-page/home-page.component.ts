31b35109111055ad5278624f0225b1c6fe0e7a9a
Mamello <mmoshoeshoe@webelieve.co.za>
2024-11-26 17:54:36 +0200

    add features section

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { FeaturesComponent } from '../../components/features/features.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [
-    BannerComponent
+    BannerComponent,
+    FeaturesComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'

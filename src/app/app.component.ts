3c8704cd6bf07f9ae963bf939d4f27bc0bd736b3
Mamello <mmoshoeshoe@webelieve.co.za>
2024-12-13 10:23:12 +0200

    add cookie warning to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CookieWarningComponent } from '../../src/app/components/cookie-warning/cookie-warning.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    CookieWarningComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
